ecd9e15c832ca62fb32902acc8392947c2031ae8
Yvann <yvann.josso@gmail.com>
2017-02-08 18:02:14 +0100

    Question 5

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,12 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller("poke",function($scope){
+    $scope.idPk = 5
+    $scope.listpoke=[
+        {idPk:1,nom:'pika'},
+        {idPk:2,nom:'pichu'},
+        {idPk:3,nom:'rondoudou'}
+    ]
+});
