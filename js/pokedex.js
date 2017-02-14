22d6b19f9d8ef10f89e68bc21cdb63bbbbea8a1a
Yvann <yvann.josso@gmail.com>
2017-02-14 12:05:32 +0100

    Q10-Q11

--- js/pokedex.js
+++ js/pokedex.js
@@ -16,3 +16,31 @@ pokeApp.controller("poke",function($scope,$log){
     ];
     $scope.$log = $log;
 });
+
+pokeApp.controller("pokeApi",function($scope,$log,$http){
+$http({
+  method: 'GET',
+  url: 'http://pokeapi.co/api/v2/pokemon/'
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    $scope.listpoke = response.data.results;
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+  $scope.$log = $log;
+});
+
+pokeApp.factory("factoryPoke",function($resource){
+    return $resource("http://pokeapi.co/api/v2/pokemon/:id/");
+});
+
+pokeApp.controller("controlerPokeApi",["factoryPoke",function(factoryPoke){
+    var pokemon = factoryPoke.get({id:2});
+    pokemon.$promise.then(function(data){
+        console.log(data);
+    },function(reason){
+        console.log("Raté"+ reason);
+    });
+}]);
